77bae4296609d00b10bba97ba02d9c3a4abd4251
Rounak Agrawal <rounak1430@gmail.com>
2023-10-02 20:05:50 +0530

    bubble sort algorithm added

---
+++ sorting-algos/bubble-sort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                int temp = a[j + 1];
+                a[j + 1] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    return 0;
+}

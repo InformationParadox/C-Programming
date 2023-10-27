c4ce158a917691667ae6b5aa63cc3255fe87dbed
Aditya <aditya.chaudhary1558@gmail.com>
2023-10-27 15:21:39 +0530

    add serach in 2d matrix.cpp

---
+++ Search2D.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+    bool searchMatrix(vector<vector<int>> &matrix, int target)
+    {
+        int row = 0;
+        int col = matrix[0].size() - 1;
+
+        while (row < matrix.size() && col >= 0)
+        {
+            if (matrix[row][col] == target)
+            {
+                return true;
+            }
+            else if (matrix[row][col] > target)
+            {
+                col--;
+            }
+            else
+            {
+                row++;
+            }
+        }
+
+        return false;
+    }
+};
+
+int main()
+{
+    Solution obj;
+    vector<vector<int>> matrix;
+    int target;
+
+    obj.searchMatrix(matrix, target);
+}
